30ea13c8c30c6f7c5140cc7430ecd3ba02a3050f
root <8907060@qq.com>
2023-09-23 22:08:45 +0800

    更新多语言键

--- src/Util.Platform.Domain.Identity/Services/Implements/UserManager.cs
+++ src/Util.Platform.Domain.Identity/Services/Implements/UserManager.cs
@@ -3,18 +3,19 @@ using Util.Platform.Domain.Models;
 using Util.Platform.Domain.Repositories;
 using Util.Platform.Domain.Services.Abstractions;
 
-namespace Util.Platform.Domain.Services.Implements; 
+namespace Util.Platform.Domain.Services.Implements;
 
 /// <summary>
 /// 用户服务
 /// </summary>
-public class UserManager : UserManagerBase<User,Role>,IUserManager {
+public class UserManager : UserManagerBase<User, Role>, IUserManager {
     /// <summary>
     /// 初始化用户服务
     /// </summary>
     /// <param name="userManager">Identity用户服务</param>
     /// <param name="userRepository">用户仓储</param>
-    public UserManager( IdentityUserManager userManager, IUserRepository userRepository ) 
-        : base(userManager, userRepository){
+    /// <param name="ipAccessor">Ip访问器</param>
+    public UserManager( IdentityUserManager userManager, IUserRepository userRepository, IIpAccessor ipAccessor )
+        : base( userManager, userRepository, ipAccessor ) {
     }
 }
