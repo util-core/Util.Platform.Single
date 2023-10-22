d078971ae0013cf2199fd0148a8f8133deca9a08
root <8907060@qq.com>
2023-10-22 13:32:31 +0800

    更新webpack模块联合配置

--- src/Util.Platform.Ui.Main/ClientApp/webpack.config.js
+++ src/Util.Platform.Ui.Main/ClientApp/webpack.config.js
@@ -2,6 +2,6 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 
 module.exports = withModuleFederationPlugin({
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: false })
+        ...shareAll({ singleton: true, strictVersion: false, requiredVersion: 'auto' })
   }
 });
