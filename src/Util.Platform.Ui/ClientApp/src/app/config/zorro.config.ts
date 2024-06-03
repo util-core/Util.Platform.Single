9b8913ecfe1a53aadc378435fc47f6b1a63c5577
utilcore <8907060@qq.com>
2024-06-03 14:45:18 +0800

    <util-card> borderless 属性修改为 bordered

--- src/Util.Platform.Ui/ClientApp/src/app/config/zorro.config.ts
+++ src/Util.Platform.Ui/ClientApp/src/app/config/zorro.config.ts
@@ -2,7 +2,11 @@ import { EnvironmentProviders } from '@angular/core';
 import { NzConfig, provideNzConfig } from 'ng-zorro-antd/core/config';
 
 //ng-zorro配置
-const config: NzConfig = {};
+const config: NzConfig = {
+    descriptions: {
+        nzColumn: { xs: 1, sm: 1, md: 1, lg: 1, xl: 2, xxl: 2 }
+    }
+};
 
 /**
  * ng-zorro配置提供器
